e38e5e55838cf03f344b1c8bbb6764a58f05e1fd
Pascallesaaman <pascallesaaman25@gmail.com>
2026-09-10 14:08:26 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+let peppa = document.querySelector("#peppa");
+let peppatekst = document.querySelector("#peppatekst");
+let article2 = document.querySelector("#article2");
+
+peppa.onclick = function () {
+
+    if (peppa.style.transform === "scale(1.2)") {
+        peppa.style.transform = "scale(1)";
+        peppatekst.innerHTML = "Klik op Peppa!";
+        article2.style.backgroundColor = "#00b4eb";
+        article2.style.color = "white";
+    } else {
+        peppa.style.transform = "scale(1.2)";
+        peppatekst.innerHTML = "Peppa is blij!";
+        article2.style.backgroundColor = "#f288b8";
+        article2.style.color = "yellow";
+    }
+
+};
+
+let george = document.querySelector("#george");
+
+george.onclick = function () {
+
+    if (george.style.transform === "scale(1.2)") {
+        george.style.transform = "scale(1)";
+        george.innerHTML = "Peppa";
+    } else {
+        george.style.transform = "scale(1.2)";
+        george.innerHTML = "Peppa is blij!";
+    }
+
+};
